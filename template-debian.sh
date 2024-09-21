32a602c8f3fb0a88a8efd3338b2583cfdaadca15
Karl Stenerud <kstenerud@gmail.com>
2024-09-21 19:31:10 +0200

    Turn off network tweaks

--- template-debian.sh
+++ template-debian.sh
@@ -21,7 +21,7 @@ pct create $INSTANCE_CT local:vztmpl/debian-12-standard_12.2-1_amd64.tar.zst \
     --unprivileged 1
 
 # Force apt to use ipv4
-echo 'Acquire::ForceIPv4 "true";' | pct exec $INSTANCE_CT -- tee /etc/apt/apt.conf.d/99force-ipv4
+# echo 'Acquire::ForceIPv4 "true";' | pct exec $INSTANCE_CT -- tee /etc/apt/apt.conf.d/99force-ipv4
 # echo 'precedence ::ffff:0:0/96  100' | pct exec $INSTANCE_CT -- tee -a /etc/gai.conf
 
 # Bugfix: Stop container from holding up network trying to get a dhcpv6 lease
@@ -31,7 +31,7 @@ echo 'Acquire::ForceIPv4 "true";' | pct exec $INSTANCE_CT -- tee /etc/apt/apt.co
 # ' | pct exec $INSTANCE_CT -- tee -a /etc/sysctl.conf
 
 # Make sure DHCP client sends the correct identifier
-echo "send dhcp-client-identifier = hardware;" | pct exec $INSTANCE_CT -- tee -a /etc/dhcp/dhclient.conf
+# echo "send dhcp-client-identifier = hardware;" | pct exec $INSTANCE_CT -- tee -a /etc/dhcp/dhclient.conf
 
 # Apt defaults
 echo 'APT::Get::Assume-Yes "true";
@@ -46,11 +46,11 @@ pct stop $INSTANCE_CT
 pct start $INSTANCE_CT
 
 # Use a fast mirror
-pct exec $INSTANCE_CT -- sed -i 's/deb.debian.org/ftp2.de.debian.org/g' /etc/apt/sources.list
+# pct exec $INSTANCE_CT -- sed -i 's/deb.debian.org/ftp2.de.debian.org/g' /etc/apt/sources.list
 
 # Extra mirror
-echo "deb http://ftp.uni-mainz.de/debian bookworm main contrib non-free non-free-firmware
-deb http://ftp.uni-mainz.de/debian bookworm-updates main contrib non-free non-free-firmware" | pct exec $INSTANCE_CT -- tee /etc/apt/sources.list.d/ftp.uni-mainz.de.list
+# echo "deb http://ftp.uni-mainz.de/debian bookworm main contrib non-free non-free-firmware
+# deb http://ftp.uni-mainz.de/debian bookworm-updates main contrib non-free non-free-firmware" | pct exec $INSTANCE_CT -- tee /etc/apt/sources.list.d/ftp.uni-mainz.de.list
 
 pct exec $INSTANCE_CT -- sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 pct exec $INSTANCE_CT -- locale-gen
@@ -62,4 +62,3 @@ pct exec $INSTANCE_CT -- apt dist-upgrade -y
 
 pct stop $INSTANCE_CT
 pct template $INSTANCE_CT
-
